b47aa6f113495682bfc106124b1c8f77e6acc9db
JuanRB09 <65874674+JuanRB09@users.noreply.github.com>
2023-04-12 18:33:03 -0600

    SplashScreen de app

---
+++ app/src/main/java/it/grp/mameti/SplashScreen/SplashActivity.kt
@@ -0,0 +1,14 @@
+package it.grp.mameti.SplashScreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.grp.mameti.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java)) //MANDA LLAMAR EL MAIN AL TERMINAR EL SPLASH
+        finish()
+    }
+}
