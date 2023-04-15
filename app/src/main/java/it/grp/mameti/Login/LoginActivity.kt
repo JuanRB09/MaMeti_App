48de01e62258fad3233162e2da730a172deb0901
JuanRB09 <65874674+JuanRB09@users.noreply.github.com>
2023-04-15 15:38:55 -0600

    Login app 1

---
+++ app/src/main/java/it/grp/mameti/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package it.grp.mameti.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.grp.mameti.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
