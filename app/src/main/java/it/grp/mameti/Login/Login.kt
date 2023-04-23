7024831c8bbfc336fe3e6e3e13b94c06df9204b9
JuanRB09 <65874674+JuanRB09@users.noreply.github.com>
2023-04-22 20:25:59 -0600

    Listo metodo de crear y logear con correo

--- app/src/main/java/it/grp/mameti/Login/Login.kt
+++ app/src/main/java/it/grp/mameti/Login/Login.kt
@@ -5,14 +5,85 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 import it.grp.mameti.CreateProfiles.CreateHumanProfile
 import it.grp.mameti.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        //val btLogin: Button = findViewById(R.id.btLogin) //EJEMPLO DE DECLARACION DE UN COMPONENTE
+
+        //EVENTOS DE GOOGLE ANALYTICS
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa")
+        analytics.logEvent("InitScreen",bundle)
+
+        acceso()
+    }
+
+    //AUTENTICACION CON FIREBASE (CORREO)
+    private fun acceso() {
+        title = "Autenticación"
+        //SOLO TOMAR UNO PARA PROBAR
+        //PARA REGISTRAR
+//        btLogin.setOnClickListener{
+//            if(etUsuario.text.isNotEmpty() && etContrasena.text.isNotBlank()){
+//                FirebaseAuth.getInstance().createUserWithEmailAndPassword(etUsuario.text.toString(),
+//                    etContrasena.text.toString()).addOnCompleteListener(){
+//                    if (it.isSuccessful){
+//                        startActivity(Intent(this, CreateHumanProfile::class.java))
+//                        finish()
+//                    }else{
+//                        alerta_noAuth()
+//                    }
+//                }
+//            }else{
+//              alerta_noData()
+//             }
+//        }
+
+        //PARA LOGEAR
+//        btLogin.setOnClickListener{
+//            if(etUsuario.text.isNotEmpty() && etContrasena.text.isNotBlank()){
+//                FirebaseAuth.getInstance().signInWithEmailAndPassword(etUsuario.text.toString(),
+//                    etContrasena.text.toString()).addOnCompleteListener(){
+//                    if (it.isSuccessful){
+//                        startActivity(Intent(this, CreateHumanProfile::class.java))
+//                        finish()
+//                    }else{
+//                        alerta_noAuth()
+//                    }
+//                }
+//            }else{
+//                alerta_noData()
+//            }
+//        }
+
+    }
+    //METODOS COMPLEMENTO: MUESTRA DE ALERTAS
+    private fun alerta_noAuth(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("¡Error!")
+        builder.setMessage("No se ha podido autenticar al usuario.")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun alerta_noData(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("¡Error!")
+        builder.setMessage("Complete ambos campos.")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 
     //NAVEGAR A LAS DIFERENTES PANTALLAS
@@ -24,3 +95,20 @@ class Login : AppCompatActivity() {
     }
 
 }
+
+    //CODIGO DE EJEMPLO PARA PASAR COMO PARAMETRO EL CORREO Y EL PROVEEDOR DEL MISMO
+//    private fun principal(usuario: String, proveedor: ProviderType){
+//        val homeIntent = Intent(this,CreateHumanProfile::class.java).apply {
+//            putExtra("usuario", usuario)
+//            putExtra("proveedor", proveedor.name)
+//        }
+//        startActivity(homeIntent)
+//
+//        //EN EL IF DE acceso()
+//        //principal(it.result?.user?.email ?:"", ProviderType.BASIC)
+//
+//    }
+//    //EN LA ACTIVIDAD A DONDE SERAN REDIRIGIDOS LOS DATOS
+//    enum class ProviderType{
+//        BASIC
+//    }
