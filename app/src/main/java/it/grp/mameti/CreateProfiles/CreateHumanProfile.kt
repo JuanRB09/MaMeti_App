c1051dff22c5e0c13331e7c413f54eae74c27607
JuanRB09 <65874674+JuanRB09@users.noreply.github.com>
2023-05-02 12:35:04 -0600

    Crear el usuario funciona correctamente

--- app/src/main/java/it/grp/mameti/CreateProfiles/CreateHumanProfile.kt
+++ app/src/main/java/it/grp/mameti/CreateProfiles/CreateHumanProfile.kt
@@ -1,40 +1,83 @@
 package it.grp.mameti.CreateProfiles
 
 import android.content.DialogInterface
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import it.grp.mameti.R
+import it.grp.mameti.SQLite.SQLiteHelper
 import kotlinx.android.synthetic.main.activity_create_human_profile.*
 
 class CreateHumanProfile : AppCompatActivity() {
+
+    lateinit var mametiDBHelper: SQLiteHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_human_profile)
         supportActionBar?.hide()
+
+        mametiDBHelper = SQLiteHelper(this)
+        noEmpty()
+    }
+
+    //VERIFICAR DATOS VACIOS
+    private fun noEmpty(){
+        title = "Verificacion"
+        btnRegistro.setOnClickListener(){
+            if (etNombres.text.isNotEmpty() &&
+                etApellidoPaterno.text.isNotEmpty() &&
+                etApellidoMaterno.text.isNotEmpty() &&
+                etCorreo.text.isNotEmpty() &&
+                etTelefono.text.isNotEmpty() &&
+                etAuthPass1.text.isNotEmpty() &&
+                etAuthPass2.text.isNotEmpty()) {
+                equalsPass()
+            }else{
+                alerta_noEmpty()
+            }
+        }
+    }
+
+    //VERIFICA QUE LAS CONTRASÑEAS SEAN IGUALES
+    private fun equalsPass(){
+        if (etAuthPass1.text.toString() == etAuthPass2.text.toString()) {
+            alertProfile()
+        }else{
+            alerta_noEqualsPass()
+        }
     }
 
     private fun alertProfile(){
-        val builder = AlertDialog.Builder(this, R.style.SplashTheme)
+        val builder = AlertDialog.Builder(this, R.style.AlertDialog)
         builder.setTitle("¡Atención!")
-        builder.setMessage("Recuerda tu e-Mail y Contraseña, ya que serán tus datos de inicio" +
-                "de sesión.")
+        builder.setMessage("Recuerda tu e-Mail y Contraseña, ya que serán tus datos de inicio de sesión.")
+
         builder.apply {
             setPositiveButton(R.string.ok,
                 DialogInterface.OnClickListener { dialog, btnAceptar ->
-                    btnRegistro.setOnClickListener{
-                        if(etCorreo.text.isNotEmpty() && etAuthPass2.text.isNotEmpty()){
-                            FirebaseAuth.getInstance().createUserWithEmailAndPassword(etCorreo.text.toString(),
-                                etAuthPass2.text.toString()).addOnCompleteListener(){
-                                if (it.isSuccessful){
-                                    finish()
-                                }else{
-                                    alerta_noAuth()
-                                }
-                            }
+                    //SI NO ESTAN VACIOS Y AMBAS CONTRASEÑAS SON IGUALES INICIA EL REGISTRO DE LA INFORMACION EN LA BASE DE DATOS
+                    mametiDBHelper.crearHumano1(
+                        etNombres.text.toString(),
+                        etApellidoPaterno.text.toString(),
+                        etApellidoMaterno.text.toString(),
+                        etCorreo.text.toString(),
+                        etTelefono.text.toString(),
+                        etAuthPass2.text.toString(),
+                    )
+
+                    //SI NO ESTAN VACIOS INICIA EL REGISTRO EN FIREBASE
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                        etCorreo.text.toString(),
+                        etAuthPass2.text.toString()
+                    ).addOnCompleteListener() {
+                        if (it.isSuccessful) {
+                            Toast.makeText(this@CreateHumanProfile,"¡Perfil creado con exito!", Toast.LENGTH_SHORT).show()
+                            finish()
+                        } else {
+                            alerta_noAuth()
                         }
                     }
                 })
@@ -42,17 +85,36 @@ class CreateHumanProfile : AppCompatActivity() {
                 DialogInterface.OnClickListener { dialog, btnCancelar ->
                     dialog.dismiss()
                 })
-
         }
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
+
     private fun alerta_noAuth(){
-        val builder = AlertDialog.Builder(this)
+        val builder = AlertDialog.Builder(this, R.style.AlertDialog)
         builder.setTitle("¡Error!")
-        builder.setMessage("No se ha podido autenticar al usuario.")
+        builder.setMessage("No se ha podido autenticar al usuario (contraseña muy corta).")
         builder.setPositiveButton("Aceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
+
+    private fun alerta_noEmpty(){
+        val builder = AlertDialog.Builder(this, R.style.AlertDialog)
+        builder.setTitle("¡Error!")
+        builder.setMessage("Rellena los campos vacios.")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun alerta_noEqualsPass(){
+        val builder = AlertDialog.Builder(this, R.style.AlertDialog)
+        builder.setTitle("¡Error!")
+        builder.setMessage("Verifica las contraseñas.")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
 }
--- app/src/main/java/it/grp/mameti/Dashboard/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/it/grp/mameti/Dashboard/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Pantalla del humano"
     }
     val text: LiveData<String> = _text
 }
